ceee7ef8e6668a4ebfd94805fc79e52b781e8b87
BeingRishabh <43031990+BeingRishabh@users.noreply.github.com>
2018-09-20 16:19:05 +0530

    Add files via upload

---
+++ marks.py
@@ -0,0 +1,13 @@
+a=int(input("enter the mark of student "))
+if a>90:
+	print (a,"the student has got A+ grade")
+elif a>80:
+	print(a,"the student has got A grade")
+elif a>70:
+	print(a,"the student has got B+ grade")
+elif a>60:
+	print("the student has got B grade")
+elif a>50:
+	print("the student has got C grade")
+else:
+	print("the student has failed :(")
